201ed67b86c7907c57b77c6c12e8c8e0ded22422
dawitstibebu <95065324+dawitstibebu@users.noreply.github.com>
2022-03-10 22:27:05 -0600

    Update future.js

--- week07/fortuneTeller/future.js
+++ week07/fortuneTeller/future.js
@@ -7,7 +7,7 @@ var numKikds = [0,3,5,1];
 var xx = Math.floor(Math.random()*4);
 
 function tellFortune(job, city, spouse, numKids) {
-    document.write("You will be a " + jobTitle + " in " + location + " and married to ' + partner + " " + " with " + numKids + " kids.");
+    document.write("You will be a " + job + " in " + city + " and married to ' + spouse + " " + " with " + numKids + " kids.");
 }
 
 fortune(job[xx], city[xx], spouse[xx], numKids[xx]);
